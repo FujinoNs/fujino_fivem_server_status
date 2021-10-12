0de7d41152210c9cd7e0c3f74a12bba44862e07c
Fujino N's <67798965+FujinoNs@users.noreply.github.com>
2021-10-12 15:53:26 +0700

    1.0.0

---
+++ fujino_fivem_server_status/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author "Fujino N's"
+description 'Fujino FiveM Server Status Create by Kroekchai KC (Fujino Ns) - https://fujinons.web.app/web/'
+version '1.0.0'
+
+server_scripts {
+  'server.lua',
+  'data.lua'
+}
